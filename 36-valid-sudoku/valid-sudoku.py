b994d6797df5a4afe9512f97540a08c32980f7dc
Hoth Stephen Bar Jock <hothbar7@gmail.com>
2025-06-08 18:53:09 -0400

    Time: 7 ms (38.11%) | Memory: 17.9 MB (43.61%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -6,12 +6,13 @@ class Solution:
 
         for r in range(9):
             for c in range(9):
+                val = board[r][c]
                 if board[r][c] == ".":
                     continue
-                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]:
+                if val in rows[r] or val in cols[c] or val in squares[(r//3, c//3)]:
                     return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r//3, c//3)].add(board[r][c])
+                cols[c].add(val)
+                rows[r].add(val)
+                squares[(r//3, c//3)].add(val)
         return True
         
