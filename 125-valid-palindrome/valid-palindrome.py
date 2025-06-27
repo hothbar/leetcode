4166dba9200c3f1107d53f3e6af017f77e0498f8
Hoth Stephen Bar Jock <hothbar7@gmail.com>
2025-06-26 23:19:10 -0400

    Time: 12 ms (18.83%) | Memory: 23.2 MB (10.66%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,7 +3,4 @@ class Solution:
         first = ''.join(c.lower() for c in s if c.isalnum())
         second = first[::-1]
 
-        if first == second:
-            return True
-        else:
-            return False
+        return first == second
